9fe6608d3de2fa3c7ad886710e92afc8770ab1bc
David Nunes <51808846+Dnuns@users.noreply.github.com>
2024-04-08 11:04:06 -0100

    fix commit

---
+++ mu-plugins/university-post-types.php
@@ -0,0 +1,20 @@
+<?php
+
+function university_post_types(){
+    register_post_type('event', array(
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'sigular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'university_post_types');
+
+?>
